f0a0b6af2d8965116bd44268437d675dfbf5f996
Anil Yadav <anilyadavjnt@gmail.com>
2026-08-13 20:04:40 +0530

    Initial Commit

---
+++ MovieTicketBookingUITests/MovieTicketBookingUITestsLaunchTests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieTicketBookingUITestsLaunchTests.swift
+//  MovieTicketBooking
+//
+//  Created by Anil Yadav on 13/08/26.
+//  Email: anilyadavjnt@gmail.com
+//  Contact No: +91-975211420
+	
+
+import XCTest
+
+final class MovieTicketBookingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
